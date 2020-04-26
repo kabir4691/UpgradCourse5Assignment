51a08ee365dafc88101c2395721f57ffd9a7b740
imranbm9 <imranbm9@gmail.com>
2020-04-26 12:33:07 +0530

    Adding question and answer entity

---
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
@@ -0,0 +1,78 @@
+package com.upgrad.quora.service.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "question", schema = "public")
+public class QuestionEntity implements Serializable {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @Size(max = 200)
+    private String uuid;
+
+
+    @Size(max = 500)
+    @NotNull
+    @Column(name = "CONTENT")
+    private String content;
+
+
+    @Column(name = "DATE")
+    private Date date;
+
+    @ManyToOne
+    @Column(name = "ID")
+    private UserEntity userId;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public UserEntity getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UserEntity userId) {
+        this.userId = userId;
+    }
+
+
+}
